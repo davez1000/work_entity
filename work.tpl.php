5582e948294e553f545b7b116778b13668624ff2
Dave Brown <davez1000@gmail.com>
2012-01-30 22:40:29 +0000

    Enhanced, rules, field API instances, and other logic / templating.

--- work.tpl.php
+++ work.tpl.php
@@ -3,48 +3,91 @@
   $content = $element->content;
 ?>
 
-<div>Title: <h2><?php echo render($content['title']); ?></div></h2>
-
-<?php if (isset($content['alpha_title'])): ?>
-  <div>Alpha title: <h3><?php echo render($content['alpha_title']); ?></h3></div>
-<?php endif; ?>
+<div>title:<?php echo render($content['title']); ?></div>
 
 <?php if (isset($content['production_date'])): ?>
-  <div>Production date: <h3><?php echo render($content['production_date']); ?></h3></div>
+  <div>production date:<?php echo render($content['production_date']); ?></div>
 <?php endif; ?>
 
-<?php if (isset($content['alternative_titles'])): ?>
-  <div>Alternative titles:</div><div><h3><?php echo render($content['alternative_titles']); ?></h3></div>
+<?php if (isset($content['identifying_title'])): ?>
+  <div>identifying_title: <?php echo render($content['identifying_title']); ?></div>
 <?php endif; ?>
+  
+<?php if (isset($content['alternate_title'])): ?>
+  <div>alternate_title: <?php echo render($content['alternate_title']); ?></div>
+<?php endif; ?>
+  
+<?php if (isset($content['introduction_summary'])): ?>
+  <div>introduction_summary: <?php echo render($content['introduction_summary']); ?></div>
+<?php endif; ?>
+<?php if (isset($content['introduction'])): ?>
+  <div>introduction: <?php echo render($content['introduction']); ?></div>
+<?php endif; ?>
+
+<?php if (isset($content['synopsis'])): ?>
+  <div>synopsis: <?php echo render($content['synopsis']); ?></div>
+<?php endif; ?>
+
+<?php if (isset($content['external_links'])): ?>
+  <div>External links:</div>
+  <?php foreach($content['external_links'] as $link): ?>
+    <div><?php echo $link; ?></div>
+  <?php endforeach; ?>
+<?php endif; ?>
+
+<?php if (isset($content['one_liner'])): ?>
+  <div>one_liner: <?php echo render($content['one_liner']); ?></div>
+<?php endif; ?>
+  
+<?php if (isset($content['primary_image'])): ?>
+  <div>image: <?php echo theme('image_style', array('style_name' => 'sys_common_gallery_image', 'path' => render($content['primary_image']))); ?></div>
+  <div>image (URL only): <?php echo image_style_url('sys_common_gallery_image', render($content['primary_image'])); ?></div>
+<?php endif; ?>
+  
+<?php if (isset($content['year'])): ?>
+  <div>year: <?php echo render($content['year']); ?></div>
+<?php endif; ?>
+
+<?php if (isset($content['work_category'])): ?>
+  <div>work_category: <?php echo render($content['work_category']); ?></div>
+<?php endif; ?>  
+
+
+<?php // if (isset($content['alpha_title'])): ?>
+  <?php // echo render($content['alpha_title']); ?>
+<?php // endif; ?>
+
+
+
+<?php // if (isset($content['alternative_titles'])): ?>
+  <?php // echo render($content['alternative_titles']); ?>
+<?php // endif; ?>
 
  
-  
 <!-- get genre terms -->
+<h3>Genres</h3>
 <?php if (isset($content['terms']['cid_genre'])): ?>
-  <div><h3>Genres</h3></div>
-  <div>
   <?php foreach($content['terms']['cid_genre'] as $term): ?>
-    <div><?php echo $term['link']; ?></div>
+    <?php echo $term; ?><br />
   <?php endforeach; ?>
-  </div>
+ 
 <?php endif; ?>
 
   
 <!-- get subject terms -->
+<h3>Subjects</h3>
 <?php if (isset($content['terms']['subject'])): ?>
-  <div><h3>Subjects</h3></div>
-  <div>
   <?php foreach($content['terms']['subject'] as $term): ?>
-    <div><?php echo $term['link']; ?></div>
+    <?php echo $term; ?><br />
   <?php endforeach; ?>
-  </div>
 <?php endif; ?>
 
   
   
-<!-- related works and agents -->
+<!-- related works -->
+<h3>Related Works</h3>
 <?php if (!empty($content['relations']['work'])): ?>
-  <div><h3>Related Works</h3></div>
+  
   <div>
   <?php foreach($content['relations']['work'] as $relation): ?>
     <?php echo $relation; ?><br />
@@ -52,8 +95,11 @@
   </div>
 <?php endif; ?>
 
+
+<!-- related agents -->
+<h3>Related Agents</h3>
 <?php if (!empty($content['relations']['agent'])): ?>
-  <div><h3>Related Agents</h3></div>
+
   <div>
   <?php foreach($content['relations']['agent'] as $relation): ?>
     <?php echo $relation; ?><br />
