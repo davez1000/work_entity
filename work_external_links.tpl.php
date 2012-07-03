f36a9692a7b95ccfea74d6d1256f152bbb3bb572
Dave Brown <davez1000@gmail.com>
2012-07-03 08:47:29 +0100

    Latest update

---
+++ work_external_links.tpl.php
@@ -0,0 +1,7 @@
+<div class="work-external-links">
+  <ul>
+    <?php foreach($links as $link): ?>
+      <li><?php echo $link; ?></li>
+    <?php endforeach; ?>
+  </ul>
+</div>
---
+++ work_subjects.tpl.php
@@ -0,0 +1,7 @@
+<div class="work-subjects">
+  <ul>
+    <?php foreach($subjects as $subject): ?>
+      <li><?php echo $subject; ?></li>
+    <?php endforeach; ?>
+  </ul>
+</div>
